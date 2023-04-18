d8e86bec5a0c8ac156ebe739368d8754ad75d431
Mercy Dare <daremercy1@gmail.com>
2023-04-18 12:17:49 +0100

    gpt3 web

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './possibility.css'
+
+import PossibilityImage from '../../assets/possibility.png'
+
+const Possibility = () => {
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={PossibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>
+        request early access to get started
+        </h4>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et assumenda rerum harum. Minus reiciendis culpa, nisi autem saepe repellat optio.</p>
+        <h4>Request Early Access to Get started</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Possibility
